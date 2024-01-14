acc057e17168fb765aa08c3d9ef35a14d655cb67
MinhDucNguyen9705 <nguyenminhduc9705@gmail.com>
2024-01-14 11:26:27 +0700

    Address to District and Modulize district_extract

--- district_extract.py
+++ district_extract.py
@@ -1,59 +1,162 @@
-import json 
-import requests
-from bs4 import BeautifulSoup 
-from unidecode import unidecode
+def get_district_list():
+    import requests
+    from bs4 import BeautifulSoup 
+    from unidecode import unidecode
 
-# Perform web scraping to get data
-url_1 = 'https://tdkmart.com/danh-sach-cac-quan-huyen-cua-64-tinh-thanh-viet-nam-link-1'
-data = requests.get(url_1)
-soup = BeautifulSoup(data.content, 'html.parser') 
-s = soup.find('div', class_="module-content") 
-content = s.find_all('p') 
-string = str(content[2])
-district_list=[]
-district_dict=dict()
-city_map = dict()
-lst = string.split('\n')[1:-1]
-for line in lst:
-    input_string = line
-    split_string = input_string.split('\xa0\xa0 \xa0')
-    if len(split_string) >= 2:
-        city, district = split_string[1].split('\xa0\xa0 \xa0')[0].strip(), split_string[3].split('<br/>')[0].strip()
-        result_tuple = (city, district)
-        district_list.append(unidecode(district).lower())
-        district_dict[unidecode(district).lower()] = city
-        city_map[unidecode(city).lower()]=city
-    else:
-        line = ()
+    # Perform web scraping to get data
+    url_1 = 'https://tdkmart.com/danh-sach-cac-quan-huyen-cua-64-tinh-thanh-viet-nam-link-1'
+    data = requests.get(url_1)
+    soup = BeautifulSoup(data.content, 'html.parser') 
+    s = soup.find('div', class_="module-content") 
+    content = s.find_all('p') 
+    string = str(content[2])
+    district_list=[]
+    lst = string.split('\n')[1:-1]
+    for line in lst:
+        input_string = line
+        split_string = input_string.split('\xa0\xa0 \xa0')
+        if len(split_string) >= 2:
+            city, district = split_string[1].split('\xa0\xa0 \xa0')[0].strip(), split_string[3].split('<br/>')[0].strip()
+            district_list.append(unidecode(district).lower())
+        else:
+            line = ()
 
-url_2 = 'https://tdkmart.com/danh-sach-cac-quan-huyen-cua-64-tinh-thanh-viet-nam-link-2'
-data = requests.get(url_2)
-soup = BeautifulSoup(data.content, 'html.parser') 
-s = soup.find('div', class_="module-content") 
-content = s.find_all('p') 
-string = str(content[1])
-lst = string.split('\n')[1:-1]
-for line in lst:
-    input_string = line
-    split_string = input_string.split('\xa0\xa0 \xa0')
-    if len(split_string) >= 2:
-        city, district = split_string[1].split('\xa0\xa0 \xa0')[0].strip(), split_string[3].split('<br/>')[0].strip()
-        result_tuple = (city, district)
-        district_list.append(unidecode(district).lower())
-        district_dict[unidecode(district).lower()] = city
-        city_map[unidecode(city).lower()]=city
-    else:
-        line = ()
+    url_2 = 'https://tdkmart.com/danh-sach-cac-quan-huyen-cua-64-tinh-thanh-viet-nam-link-2'
+    data = requests.get(url_2)
+    soup = BeautifulSoup(data.content, 'html.parser') 
+    s = soup.find('div', class_="module-content") 
+    content = s.find_all('p') 
+    string = str(content[1])
+    lst = string.split('\n')[1:-1]
+    for line in lst:
+        input_string = line
+        split_string = input_string.split('\xa0\xa0 \xa0')
+        if len(split_string) >= 2:
+            city, district = split_string[1].split('\xa0\xa0 \xa0')[0].strip(), split_string[3].split('<br/>')[0].strip()
+            district_list.append(unidecode(district).lower())
+        else:
+            line = ()
+    return district_list
 
-# print(district_list)
-# print(district_dict)
-# print(city_map)
+def get_district_dict():
+    import requests
+    from bs4 import BeautifulSoup 
+    from unidecode import unidecode
+
+    # Perform web scraping to get data
+    url_1 = 'https://tdkmart.com/danh-sach-cac-quan-huyen-cua-64-tinh-thanh-viet-nam-link-1'
+    data = requests.get(url_1)
+    soup = BeautifulSoup(data.content, 'html.parser') 
+    s = soup.find('div', class_="module-content") 
+    content = s.find_all('p') 
+    string = str(content[2])
+    district_dict=dict()
+    lst = string.split('\n')[1:-1]
+    for line in lst:
+        input_string = line
+        split_string = input_string.split('\xa0\xa0 \xa0')
+        if len(split_string) >= 2:
+            city, district = split_string[1].split('\xa0\xa0 \xa0')[0].strip(), split_string[3].split('<br/>')[0].strip()
+            district_dict[unidecode(district).lower()] = city
+        else:
+            line = ()
+
+    url_2 = 'https://tdkmart.com/danh-sach-cac-quan-huyen-cua-64-tinh-thanh-viet-nam-link-2'
+    data = requests.get(url_2)
+    soup = BeautifulSoup(data.content, 'html.parser') 
+    s = soup.find('div', class_="module-content") 
+    content = s.find_all('p') 
+    string = str(content[1])
+    lst = string.split('\n')[1:-1]
+    for line in lst:
+        input_string = line
+        split_string = input_string.split('\xa0\xa0 \xa0')
+        if len(split_string) >= 2:
+            city, district = split_string[1].split('\xa0\xa0 \xa0')[0].strip(), split_string[3].split('<br/>')[0].strip()
+            district_dict[unidecode(district).lower()] = city
+        else:
+            line = ()
+    return district_dict
+
+def get_city_map():
+    import requests
+    from bs4 import BeautifulSoup 
+    from unidecode import unidecode
+
+    # Perform web scraping to get data
+    url_1 = 'https://tdkmart.com/danh-sach-cac-quan-huyen-cua-64-tinh-thanh-viet-nam-link-1'
+    data = requests.get(url_1)
+    soup = BeautifulSoup(data.content, 'html.parser') 
+    s = soup.find('div', class_="module-content") 
+    content = s.find_all('p') 
+    string = str(content[2])
+    city_map = dict()
+    lst = string.split('\n')[1:-1]
+    for line in lst:
+        input_string = line
+        split_string = input_string.split('\xa0\xa0 \xa0')
+        if len(split_string) >= 2:
+            city, district = split_string[1].split('\xa0\xa0 \xa0')[0].strip(), split_string[3].split('<br/>')[0].strip()
+            city_map[unidecode(city).lower()]=city
+        else:
+            line = ()
+
+    url_2 = 'https://tdkmart.com/danh-sach-cac-quan-huyen-cua-64-tinh-thanh-viet-nam-link-2'
+    data = requests.get(url_2)
+    soup = BeautifulSoup(data.content, 'html.parser') 
+    s = soup.find('div', class_="module-content") 
+    content = s.find_all('p') 
+    string = str(content[1])
+    lst = string.split('\n')[1:-1]
+    for line in lst:
+        input_string = line
+        split_string = input_string.split('\xa0\xa0 \xa0')
+        if len(split_string) >= 2:
+            city, district = split_string[1].split('\xa0\xa0 \xa0')[0].strip(), split_string[3].split('<br/>')[0].strip()
+            city_map[unidecode(city).lower()]=city
+        else:
+            line = ()
+    return city_map
         
-import pandas as pd 
+def get_avenues_list():
+    import pandas as pd 
+    import numpy as np
+    from unidecode import unidecode
+    df = pd.read_csv('da_nang_data.csv')
+    column_dict = {column: df[column].to_list() for column in df.columns[1:-1]}
+    avenues_map = dict()
+    avenues_list = []
+    for key, values in column_dict.items():
+        for value in values:
+            if value is not np.NaN:
+                # avenues_map[unidecode(key).lower()].append(unidecode(' '.join(value.split('\xa0'))).lower())
+                if 'Đường' in value or 'Phường' in value or 'Xã' in value:
+                    avenues_map[' '.join(unidecode(' '.join(value.split('\xa0'))).lower().split()[1:])]=unidecode(key).lower()
+                    avenues_list.append(' '.join(unidecode(' '.join(value.split('\xa0'))).lower().split()[1:]))
+                else:
+                    avenues_map[unidecode(' '.join(value.split('\xa0'))).lower()]=unidecode(key).lower()
+                    avenues_list.append(unidecode(' '.join(value.split('\xa0'))).lower())
+    return avenues_list
 
-df = pd.read_csv('da_nang_data.csv')
-avenues_list = df.iloc[:,1].to_list()
-for i in range (len(avenues_list)):
-    temp = ' '.join(avenues_list[i].split('\xa0'))
-    avenues_list[i] = unidecode(temp).lower()
-# print(avenues_list)
+def get_avenues_map():
+    import pandas as pd 
+    import numpy as np
+    from unidecode import unidecode
+    df = pd.read_csv('da_nang_data.csv')
+    column_dict = {column: df[column].to_list() for column in df.columns[1:-1]}
+    avenues_map = dict()
+    avenues_list = []
+    for key, values in column_dict.items():
+        for value in values:
+            if value is not np.NaN:
+                # avenues_map[unidecode(key).lower()].append(unidecode(' '.join(value.split('\xa0'))).lower())
+                if 'Đường' in value or 'Phường' in value or 'Xã' in value:
+                    avenues_map[' '.join(unidecode(' '.join(value.split('\xa0'))).lower().split()[1:])]=unidecode(key).lower()
+                    avenues_list.append(' '.join(unidecode(' '.join(value.split('\xa0'))).lower().split()[1:]))
+                else:
+                    avenues_map[unidecode(' '.join(value.split('\xa0'))).lower()]=unidecode(key).lower()
+                    avenues_list.append(unidecode(' '.join(value.split('\xa0'))).lower())
+    return avenues_map
+
+if __name__ == '__main__':
+    print(get_avenues_map())
