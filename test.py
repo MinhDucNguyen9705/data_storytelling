d369d72d8022c53831ee1a426e36e29ff2b72d5e
unknown <phamtrungkien0901@gmail.com>
2024-01-21 08:39:36 +0700

    Visualization

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ customer = pd.read_csv('customer.csv')
 ticket = pd.read_csv('ticket.csv')
 film = pd.read_csv('film.csv')
 
-print(customer.head())
-print(ticket.head())
-print(film.head())
+print(customer.columns)
+print(ticket.columns)
+print(film.columns)
 
 #hello 
